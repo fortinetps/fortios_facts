32af8c958f866d18170062f6f90b40cacc709014
fortinetps <dyao@fortinet.com>
2019-08-15 03:49:10 +0000

    simplify fortios facts module to one single file for now

--- lib/ansible/modules/network/fortios/fortios_facts.py
+++ lib/ansible/modules/network/fortios/fortios_facts.py
@@ -1,27 +1,31 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
-# Copyright 2019 Red Hat
-# GNU General Public License v3.0+
-# (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
-"""
-The module file for fortios_facts
-"""
+from __future__ import (absolute_import, division, print_function)
+# Copyright 2019 Fortinet, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
+ANSIBLE_METADATA = {'status': ['preview'],
+                    'supported_by': 'community',
+                    'metadata_version': '1.1'}
 
-ANSIBLE_METADATA = {'metadata_version': '1.1',
-                    'status': [u'preview'],
-                    'supported_by': 'network'}
-
-
-DOCUMENTATION = """
+DOCUMENTATION = '''
 ---
 module: fortios_facts
-version_added: 2.2
+version_added: "2.9"
 short_description: Get facts about fortios devices.
-extends_documentation_fragment: fortios
 description:
   - Collects facts from network devices running the fortios operating
     system. This module places the facts gathered in the fact tree keyed by the
@@ -29,151 +33,142 @@ description:
     base set of facts from the device and can enable or disable
     collection of additional facts.
 author:
-  - Ricardo Carrillo Cruz (@rcarrillocruz)
-  - Nilashish Chakraborty (@Nilashishc)
+    - Miguel Angel Munoz (@mamunozgonzalez)
+    - Nicolas Thomas (@thomnico)
+    - Don Yao (@fortinetps)
+notes:
+    - Requires fortiosapi library developed by Fortinet
+    - Run as a local_action in your playbook
+requirements:
+    - fortiosapi>=0.9.8
 options:
-  gather_subset:
-    description:
-      - When supplied, this argument will restrict the facts collected
-        to a given subset.  Possible values for this argument include
-        all, hardware, config, and interfaces.  Can specify a list of
-        values to include a larger subset.  Values can also be used
-        with an initial C(M(!)) to specify that a specific subset should
-        not be collected.
-    required: false
-    default: '!config'
-  gather_network_resources:
-    description:
-      - When supplied, this argument will restrict the facts collected
-        to a given subset. Possible values for this argument include
-        all and the resources like interfaces, lacp etc.
-        Can specify a list of values to include a larger subset. Values
-        can also be used with an initial C(M(!)) to specify that a
-        specific subset should not be collected.
-    required: false
-    choices: ['all', 'lacp', '!lacp', 'lacp_interfaces', '!lacp_interfaces']
-    version_added: "2.9"
-"""
-
-EXAMPLES = """
-# Gather all facts
-- fortios_facts:
-    gather_subset: all
-    gather_network_resources: all
-
-# Collect only the config and default facts
-- fortios_facts:
+    host:
+        description:
+            - FortiOS or FortiGate IP address.
+        type: str
+        required: false
+    username:
+        description:
+            - FortiOS or FortiGate username.
+        type: str
+        required: false
+    password:
+        description:
+            - FortiOS or FortiGate password.
+        type: str
+        default: ""
+    vdom:
+        description:
+            - Virtual domain, among those defined previously. A vdom is a
+              virtual instance of the FortiGate that can be configured and
+              used as a different unit.
+        type: str
+        default: root
+    https:
+        description:
+            - Indicates if the requests towards FortiGate must use HTTPS protocol.
+        type: bool
+        default: true
+    ssl_verify:
+        description:
+            - Ensures FortiGate certificate must be verified by a proper CA.
+        type: bool
+        default: true
     gather_subset:
-      - config
+      description:
+        - When supplied, this argument will restrict the facts collected
+          to a given subset.  Possible values for this argument include
+          all, hardware, config, and interfaces.  Can specify a list of
+          values to include a larger subset.  Values can also be used
+          with an initial C(M(!)) to specify that a specific subset should
+          not be collected.
+      required: false
+      default: 'system'
+'''
 
-# Do not collect hardware facts
-- fortios_facts:
-    gather_subset:
-      - "!hardware"
+EXAMPLES = '''
+- hosts: localhost
+  vars:
+    host: "192.168.122.40"
+    username: "admin"
+    password: ""
+    vdom: "root"
+    ssl_verify: "False"
 
-# Collect only the lacp facts
-- fortios_facts:
-    gather_subset:
-      - "!all"
-      - "!min"
-    gather_network_resources:
-      - lacp
+  tasks:
+  - name: gather system status and system interface facts
+    fortios_facts:
+      host:  "{{ host }}"
+      username: "{{ username }}"
+      password: "{{ password }}"
+      vdom:  "{{ vdom }}"
+      https: "False"
+      gather_subset:
+        - "system_status_select"
+        - "system_interface_select"
+  '''
 
-# Do not collect lacp_interfaces facts
-- fortios_facts:
-    gather_network_resources:
-      - "!lacp_interfaces"
-
-# Collect lacp and minimal default facts
-- fortios_facts:
-    gather_subset: min
-    gather_network_resources: lacp
-"""
-
-RETURN = """
-ansible_net_gather_subset:
+RETURN = '''
+build:
+  description: Build number of the fortigate image
+  returned: always
+  type: str
+  sample: '1547'
+http_method:
+  description: Last method used to provision the content into FortiGate
+  returned: always
+  type: str
+  sample: 'GET'
+name:
+  description: Name of the table used to fulfill the request
+  returned: always
+  type: str
+  sample: "firmware"
+path:
+  description: Path of the table used to fulfill the request
+  returned: always
+  type: str
+  sample: "system"
+revision:
+  description: Internal revision number
+  returned: always
+  type: str
+  sample: "17.0.2.10658"
+serial:
+  description: Serial number of the unit
+  returned: always
+  type: str
+  sample: "FGVMEVYYQT3AB5352"
+status:
+  description: Indication of the operation's result
+  returned: always
+  type: str
+  sample: "success"
+vdom:
+  description: Virtual domain used
+  returned: always
+  type: str
+  sample: "root"
+version:
+  description: Version of the FortiGate
+  returned: always
+  type: str
+  sample: "v5.6.3"
+ansible_facts:
   description: The list of fact subsets collected from the device
   returned: always
   type: list
-
-# default
-ansible_net_version:
-  description: The operating system version running on the remote device
+# system
+fortios_system_status:
+  description: The fortios basic system status information running on the remote device
   returned: always
-  type: str
-ansible_net_hostname:
-  description: The configured hostname of the device
-  returned: always
-  type: str
-ansible_net_image:
-  description: The image file the device is running
-  returned: always
-  type: str
-ansible_net_api:
-  description: The name of the transport
-  returned: always
-  type: str
-ansible_net_python_version:
-  description: The Python version Ansible controller is using
-  returned: always
-  type: str
-ansible_net_model:
-  description: The model name returned from the device
-  returned: always
-  type: str
-
-# hardware
-ansible_net_filesystems:
-  description: All file system names available on the device
-  returned: when hardware is configured
-  type: list
-ansible_net_memfree_mb:
-  description: The available free memory on the remote device in Mb
-  returned: when hardware is configured
-  type: int
-ansible_net_memtotal_mb:
-  description: The total memory on the remote device in Mb
-  returned: when hardware is configured
-  type: int
-
-# config
-ansible_net_config:
-  description: The current active config from the device
-  returned: when config is configured
-  type: str
-
-# interfaces
-ansible_net_all_ipv4_addresses:
-  description: All IPv4 addresses configured on the device
-  returned: when interfaces is configured
-  type: list
-ansible_net_all_ipv6_addresses:
-  description: All IPv6 addresses configured on the device
-  returned: when interfaces is configured
-  type: list
-ansible_net_interfaces:
-  description: A hash of all interfaces running on the system
-  returned: when interfaces is configured
-  type: dict
-ansible_net_neighbors:
-  description: The list of LLDP neighbors from the remote device
-  returned: when interfaces is configured
   type: dict
 
-# network resources
-ansible_net_gather_network_resources:
-  description: The list of fact resource subsets collected from the device
-  returned: always
-  type: list
-"""
+'''
 
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.connection import Connection
-from ansible.module_utils.network.fortios.fortios import FortiOSHandler
 from ansible.module_utils.network.fortimanager.common import FAIL_SOCKET_MSG
-from ansible.module_utils.network.fortios.fortios import fortios_argument_spec
-from ansible.module_utils.network.fortios.argspec.facts.facts import FactsArgs
-from ansible.module_utils.network.fortios.facts.facts import Facts
 from ansible.module_utils.six import iteritems
 
 
@@ -185,6 +180,18 @@ class Factbase(object):
         self.facts = dict()
 
 
+FACT_SYSTEM_SUBSETS = frozenset([
+    'system_current-admins_select',
+    'system_debug_download'
+    'system_firmware_select',
+    'system_fortimanager_status',
+    'system_ha-checksums_select',
+    'system_interface_select',
+    'system_status_select',
+    'system_time_select',
+    'system_timezong_select',
+])
+        
 class System(Factbase):
     def populate_facts(self):
         fos = self.fos
@@ -192,6 +199,13 @@ class System(Factbase):
         if self.uri.startswith(tuple(FACT_SYSTEM_SUBSETS)):
             resp = fos.monitor('system', self.uri[len('system_'):].replace('_','/'), vdom=vdom)
             self.facts.update({self.uri: resp})
+
+
+FACT_SUBSETS = dict(
+    system=System,
+)
+  
+VALID_SUBSETS = frozenset(FACT_SUBSETS.keys())
 
 
 def login(data, fos):
@@ -209,46 +223,26 @@ def login(data, fos):
     fos.login(host, username, password, verify=ssl_verify)
 
 
-
-FACT_SUBSETS = dict(
-    system=System,
-)
-  
-VALID_SUBSETS = frozenset(FACT_SUBSETS.keys())
-
-FACT_SYSTEM_SUBSETS = frozenset([
-    "system_firmware_select",
-    "system_ha-checksums_select",
-    "system_interface_select",
-    "system_status_select"
-])
-
 def main():
-    """
-    Main entry point for module execution
+    fields = {
+        "host": {"required": True, "type": "str"},
+        "username": {"required": True, "type": "str"},
+        "password": {"required": True, "type": "str", "no_log": True},
+        "vdom": {"required": False, "type": "str", "default": "root"},
+        "https": {"required": False, "type": "bool", "default": True},
+        "ssl_verify": {"required": False, "type": "bool", "default": False},
+        'gather_subset': {"required": False, "type": "list", "default": ['system']}
+    }
 
-    :returns: ansible_facts
-    """
-    spec = FactsArgs.argument_spec
-    spec.update(fortios_argument_spec)
-
-    module = AnsibleModule(argument_spec=spec,
+    module = AnsibleModule(argument_spec=fields,
                            supports_check_mode=False)
-    # warnings = ['default value for `gather_subset` '
-    #             'will be changed to `min` from `!config` v2.11 onwards']
-  
+
     legacy_mode = 'host' in module.params and module.params['host'] is not None and \
                   'username' in module.params and module.params['username'] is not None and \
                   'password' in module.params and module.params['password'] is not None
 
-    if not legacy_mode:
-        if module._socket_path:
-            connection = Connection(module._socket_path)
-            fos = FortiOSHandler(connection)
-
-            is_error, has_changed, result = fortios_user(module.params, fos)
-        else:
-            module.fail_json(**FAIL_SOCKET_MSG)
+    if not legacy_mode: # for now only support local connection mode
+        module.fail_json(**FAIL_SOCKET_MSG)
     else:
         try:
             from fortiosapi import FortiOSAPI
@@ -258,49 +252,25 @@ def main():
         fos = FortiOSAPI()
 
         login(module.params, fos)
-
         gather_subset = module.params['gather_subset']
 
         runable_subsets = set()
-        exclude_subsets = set()
     
         for subset in gather_subset:
-            if subset == 'all':
-                runable_subsets.update(VALID_SUBSETS)
-                continue
-    
-            if subset.startswith('!'):
-                subset = subset[1:]
-                if subset == 'all':
-                    exclude_subsets.update(VALID_SUBSETS)
-                    continue
-                exclude = True
-            else:
-                exclude = False
-            
             if not subset.startswith(tuple(VALID_SUBSETS)):
                 module.fail_json(msg='Subset must be one of [%s], got %s' %
                                  (', '.join(VALID_SUBSETS), subset))
     
-            if exclude:
-                for valid_subset in VALID_SUBSETS:
-                    if subset.startswith(valid_subset):
-                        exclude_subsets.add({subset: valid_subset})
-            else:
-                for valid_subset in VALID_SUBSETS:
-                    if subset.startswith(valid_subset):
-                        runable_subsets.add((subset, valid_subset))
+            for valid_subset in VALID_SUBSETS:
+                if subset.startswith(valid_subset):
+                    runable_subsets.add((subset, valid_subset))
     
         if not runable_subsets:
             runable_subsets.update(VALID_SUBSETS)
     
-        runable_subsets.difference_update(exclude_subsets)
-        # runable_subsets.add('system')
-    
         facts = dict()
         facts['gather_subset'] = list(runable_subsets)
     
-        # Create instance classes, e.g. System, Session etc.
         instances = list()
     
         for (subset, valid_subset) in runable_subsets:
@@ -312,15 +282,8 @@ def main():
             facts.update(inst.facts)
 
         fos.logout()
-    
-        ansible_facts = dict()
-    
-        for key, value in iteritems(facts):
-            # key = 'ansible_net_%s' % key
-            key = key
-            ansible_facts[key] = value
-    
-        module.exit_json(ansible_facts=ansible_facts)
+
+        module.exit_json(ansible_facts=facts)
     
 
 if __name__ == '__main__':
